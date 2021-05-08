dca225eeaa209ea42aef8b0b66b2d831f1f75d3b
Simio64 <yugipoi@gmail.com>
2021-05-08 18:28:37 -0500

    distribucion frecuentista

--- probabilidad.py
+++ probabilidad.py
@@ -29,7 +29,27 @@ import matplotlib.pyplot as plt
 def Distribucion_binomial(k,n,p):
     return factorial(n)/(factorial(k)*factorial(n-k))*pow(p,k)*pow(1-p,n-k)
 
+def DB(rango):
+    values = [0,1,2,3]
+    arr = [binomial(3,0.5) for _ in range (rango)]
+    sim = np.unique(arr,return_counts=True)[1]/len(arr)
+    teorico = [binom(3,0.5).pmf(k) for k in values]
+    plt.subplot(1,2,1)
+    plt.bar(values,sim,color='r')
+    plt.subplot(1,2,2)
+    plt.bar(values,teorico,color='b',alpha=0.5)
+    plt.show()
+    return
+
+
+#k= numero de exitos
+#n= numero de lansamientos
+#p= probabilidad de exito
+"""
 k=int(input("Numero 1"))
 n=int(input("Numero 2"))
 p=int(input("Numero 3"))
 print(Distribucion_binomial(k,n,p))
+"""
+
+arr=DB(100000)
