fdaa24f8a490bbd35298a53f740a0bcaaee26711
Simio64 <yugipoi@gmail.com>
2021-05-12 12:50:56 -0500

    distribucion normal

--- probabilidad.py
+++ probabilidad.py
@@ -20,8 +20,11 @@ marginal = de una compuesta sale una simple
 
 condicional = Recla del producto, una esta breviamente definida !!no reflejan causalidad!!
 """
+from matplotlib import colors
 import numpy as np 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from numpy.core.defchararray import array
+from numpy.core.numeric import True_ 
 import pandas as pd 
 from numpy.random import binomial
 from scipy.stats import binom 
@@ -53,11 +56,33 @@ def Distribucion_gaussiana():
     y = 1/(sigma*np.sqrt(2*np.pi))*np.exp(-0.5*pow((x-mu)/sigma,2))
     plt.plot(x,y)
     plt.show()
+
+    dist = norm(0,1)
+    y = [dist.pdf(value) for value in x] # sitribucion pobabilidad de densidad
+    plt.plot(x,y)
+    plt.show()
+
+    y = [dist.cdf(value) for value in x] #distribucion probabilidad acumulada
+    plt.plot(x,y)
+    plt.show()
     return 0
 
-def ejersisio_alas():
+def ejersisio_alas():#campana de gauss
     df = pd.read_excel('./s057.xls')
-    print(df)
+    array1=df['Normally Distributed Housefly Wing Lengths'].values[4:]
+    estimacion_dis(array1)
+    valores,distribuccion=np.unique(array1,return_counts=True)
+    plt.bar(valores,distribuccion/len(array1))
+    plt.show()
+    return 0
+
+def estimacion_dis(array1): #estimacion parametrica
+    mu = array1.mean()
+    sigma = array1.std()
+    x = np.arange(30,60,0.1)
+    dist= norm(mu,sigma)
+    y = [dist.pdf(value) for value in x]
+    plt.plot(x,y,color='r')
     return 0
 
 ejersisio_alas()
